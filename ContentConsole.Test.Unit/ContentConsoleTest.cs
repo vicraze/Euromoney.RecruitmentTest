58fd997fd7c7eefbdd54990f023cc6b58df0bf05
vicraze <vijay.alagesan@gmail.com>
2018-08-02 03:17:30 +0100

    modified code

---
+++ ContentConsole.Test.Unit/ContentConsoleTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using ContentConsole;
+using Moq;
+using NUnit.Framework;
+
+namespace ContentConsole.Test.Unit
+{
+    [TestFixture]
+    public class ContentConsoleTest
+    {
+        [Test]
+        public void Test_GetNegativeWordsCount()    //story 1 and story 4
+        {
+            // Assign
+            string input = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            int expected = 2;
+
+            // Act
+            var actual = ContentConsole.Program.GetNegativeWordsCount(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test_ChangeNegativeWordsSet_add_two()    //story 2
+        {
+            // Assign
+            string[] input  = new string[] {"worst","ugly"};
+            int expected = 6;
+
+            // Act
+            ContentConsole.Program.NegativeWords.AddRange(input);
+            int actual = Program.NegativeWords.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void Test_FilterNegativeWordFromContent()   //story 3
+        {
+            // Assign
+            string input = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            string expected = "The weather in Manchester in winter is b#d. It rains all the time - it must be h######e for people visiting.";
+
+            // Act
+            var actual = ContentConsole.Program.FilterNegativeWordFromContent(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+       
+    }
+}
--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ContentConsole
 {
@@ -6,38 +8,90 @@ namespace ContentConsole
     {
         public static void Main(string[] args)
         {
-            string bannedWord1 = "swine";
-            string bannedWord2 = "bad";
-            string bannedWord3 = "nasty";
-            string bannedWord4 = "horrible";
-
             string content =
                 "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
 
-            int badWords = 0;
-            if (content.Contains(bannedWord1))
+            string filteredcontent;
+            if (!DisableNegativeWordFiltering())
             {
-                badWords = badWords + 1;
+                filteredcontent = FilterNegativeWordFromContent(content);
+                Console.WriteLine(filteredcontent);
             }
-            if (content.Contains(bannedWord2))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord3))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord4))
-            {
-                badWords = badWords + 1;
-            }
+            else
+            { Console.WriteLine(content);  }
 
-            Console.WriteLine("Scanned the text:");
-            Console.WriteLine(content);
-            Console.WriteLine("Total Number of negative words: " + badWords);
+            Console.WriteLine("Total Number of negative words: " + GetNegativeWordsCount(content));
 
             Console.WriteLine("Press ANY key to exit.");
             Console.ReadKey();
+        }
+
+        static List<string> _negativeWords = new List<string> { "swine", "bad", "nasty", "horrible" };
+        public static List<string> NegativeWords
+        {
+            get{
+                return _negativeWords;
+            }
+            set{
+                _negativeWords = value;
+            }
+            
+        }
+
+        public static int GetNegativeWordsCount(string content)
+        {
+            var punctuations = content.Where(char.IsPunctuation).Distinct().ToArray();
+
+            foreach (var p in punctuations)
+            {
+                content = content.Replace(Char.ToString(p), " ");
+            }
+            List<string> contentList = content.Split().ToList();
+
+            return contentList.Intersect(NegativeWords).Count();
+
+        }
+
+        public static string FilterNegativeWordFromContent(string content)
+        {
+            foreach (var item in NegativeWords)
+            {
+                if (content.Contains(item))
+                {
+                    content = content.Replace(item, FilterNegativeWord(item));
+                }
+            }
+
+            return content;
+        }
+
+        private static string FilterNegativeWord(string negativeWord)
+        {
+            if (negativeWord.Length > 2)
+            {
+                string result = negativeWord.Substring(0, 1);
+                for (int i = 1; i < negativeWord.Length - 1; i++)
+                {
+                    result += "#";
+                }
+                result += negativeWord.Substring(negativeWord.Length - 1, 1);
+                return result;
+            }
+            else return negativeWord;
+
+        }
+
+        private static bool DisableNegativeWordFiltering()
+        {
+            string inputValue;
+            do
+            {
+                Console.WriteLine("Disable negative word filtering Y or N ?");
+                inputValue = Console.ReadLine();
+            } while (inputValue.ToUpper() != "Y" && inputValue.ToUpper() != "N");
+
+            return inputValue.ToUpper() == "Y" ? true : false;
+
         }
     }
 
